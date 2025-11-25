b3218ce4f6df9d3d958422f63e6de1df9ebbf87b
AliciaMontinho <aliciamontinho@gmail.com>
2025-11-25 10:20:47 +0100

    Validación visual del login

--- views/login.php
+++ views/login.php
@@ -1,27 +1,33 @@
 <!-- En funcionamiento -->
-<?php include_once '../includes/header_login.php'; ?> 
+<?php include_once '../includes/header_login.php'; ?>
 <link rel="stylesheet" href="../assets/css/login.css">
+
 <div class="contenedor ">
     <div class="card card-login shadow-lg p-5">
         <h3 class="text-center mb-4 text-primary fw-bold">Iniciar sesión</h3>
 
-        <form action="../controller/UsuarioController.php?action=login" method="POST">
+        <form id="formLogin" action="../controller/UsuarioController.php?action=login" method="POST">
 
             <div class="mb-3">
-                <!-- Si existe una cookie (Se ha seleccionado el campo recuerdam) rellenamos el campo email -->
                 <label for="email" class="form-label">Correo electrónico:</label>
-                <input type="email" name="email" class="form-control" id="email" value="<?php echo $_COOKIE['usuario_email'] ?? '' ?>">
+                <input type="email" name="email" class="form-control" id="email" 
+                       value="<?php echo $_COOKIE['usuario_email'] ?? '' ?>">
                 <div id="emailAviso" class="form-text">No compartiremos tus datos con externos.</div>
+                <div id="emailError" class="invalid-feedback d-none"></div>
             </div>
+
             <div class="mb-3">
                 <label for="password" class="form-label">Contraseña:</label>
                 <input type="password" class="form-control" name="password" id="password">
+                <div id="passwordError" class="invalid-feedback d-none"></div>
             </div>
+
             <div class="mb-3 form-check">
                 <input type="checkbox" name="recuerdame" value="1" class="form-check-input" id="recuerdame">
                 <label class="form-check-label" for="recuerdame">Recuérdame</label>
             </div>
-            <button type="submit" class="btn btn-primary">Inicio</button>
+
+            <button type="submit" class="btn btn-primary w-100">Inicio</button>
         </form>
 
         <p class="mt-4 text-center fs-6">
@@ -31,4 +37,6 @@
     </div>
 </div>
 
+<link rel="stylesheet" href="validacion_login.css">
+<script src="../assets/js/validacion_login.js"></script>
 <?php include_once '../includes/footer.php'; ?>
