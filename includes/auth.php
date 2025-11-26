963cb6a875dc2dfef8e7bf3f0ff219b5a65584f2
AliciaMontinho <aliciamontinho@gmail.com>
2025-11-26 11:34:47 +0100

    hacemos un arreglo en auth.php para que no duplique sesiones

--- includes/auth.php
+++ includes/auth.php
@@ -1,7 +1,12 @@
 <?php
-session_start();
+// Iniciar sesión solo si no hay una sesión activa (evita warnings por reiniciar)
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: login.php");
     exit;
 }
+?>
+
